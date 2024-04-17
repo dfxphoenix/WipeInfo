28c79d59fff5581731e7e299f3e69d31e1874699
dFxPhoeniX <waltercurelea@yahoo.com>
2024-04-17 16:33:02 +0300

    Version 1.2.8
    The code has been rewritten and optimized
    Some bugfixes

--- WipeInfo.cs
+++ WipeInfo.cs
@@ -3,171 +3,226 @@ using System.Collections.Generic;
 using System.Globalization;
 using System.Linq;
 using Newtonsoft.Json;
+using System.Text.RegularExpressions;
+using UnityEngine;
 
 namespace Oxide.Plugins
 {
-    [Info("Wipe Info", "dFxPhoeniX", "1.2.6")]
+    [Info("Wipe Info", "dFxPhoeniX", "1.2.8")]
     [Description("Adds the ablity to see wipe cycles")]
     public class WipeInfo : RustPlugin
     {
-        #region Fields
-        DateTime NextWipeDate;
-        Timer announceTimer;
-        #endregion
+        private string LastWipe;
+        private string NextWipe;
 
-        #region Oxide Hooks 
+        Timer announceTimer;
+
+        ////////////////////////////////////////////////////////////
+        // Oxide Hooks
+        ////////////////////////////////////////////////////////////
+
+        private void Init()
+        {
+            InitConfig();
+        }
+
         void OnServerInitialized()
         {
             LoadVariables();
 
-            LoadWipeDates();
-
-            if (config.AnnounceOnTimer)
+            if (AnnounceOnTimer)
             {
-                announceTimer = timer.Repeat((config.AnnounceTimer * 60) * 60, 0, ()=> BroadcastWipe()); 
+                announceTimer = timer.Repeat((AnnounceTimer * 60) * 60, 0, ()=> BroadcastWipe()); 
             }
         }
+
         void OnPlayerConnected(BasePlayer player)
         {
-            if (config.AnnounceOnJoin)
+            if (AnnounceOnJoin)
             {
                 cmdNextWipe(player, "", new string[0]);
             }
         }
-        #endregion
 
-        #region Functions        
-        private string LastWipe = "";
-        private string NextWipe = "";
-        private DateTime ParseTime(string time) => DateTime.ParseExact(time, config.DateFormat, CultureInfo.InvariantCulture);
-        private void LoadWipeDates()
+        ////////////////////////////////////////////////////////////
+        // General Methods
+        ////////////////////////////////////////////////////////////
+
+        private DateTime ParseTime(string time) => DateTime.ParseExact(time, DateFormat, CultureInfo.InvariantCulture);
+
+        private string NextWipeDays(string WipeDate)
         {
-            NextWipeDate = ParseTime(NextWipe);
+            DateTime wipeDateTime;
+
+            if (DateTime.TryParseExact(WipeDate, DateFormat, CultureInfo.InvariantCulture, DateTimeStyles.None, out wipeDateTime))
+            {
+                TimeSpan t = wipeDateTime.Subtract(DateTime.Today);
+                if (wipeDateTime.Date == DateTime.Today)
+                {
+                    return "Today";
+                }
+                else
+                {
+                    return string.Format("{0:D2}D ({1:D2})", t.Days, WipeDate);
+                }
+            }
+
+            return "NoDateFound";
         }
-        private string NextWipeDays(DateTime WipeDate)
-        {            
-            TimeSpan t = WipeDate.Subtract(DateTime.Now);
-            return string.Format(string.Format("{0:D2}D",t.Days));
+
+        private void LoadVariables()
+        {
+            DateTime dateTime = DateTime.Today;
+            DateTime firstDayMonth = new DateTime(dateTime.Year, dateTime.Month, 1);
+            DateTime firstDayNextMonth = firstDayMonth.AddMonths(1);
+            DateTime lastDayMonth = firstDayMonth.AddMonths(1).AddDays(-1);
+            DateTime lastDayNextMonth = firstDayMonth.AddMonths(2).AddDays(-1);
+
+            List<DateTime> datesList = new List<DateTime>();
+
+            for (DateTime day = firstDayMonth.Date; day.Date <= lastDayMonth.Date; day = day.AddDays(1))
+            {
+                datesList.Add(day);
+            }
+
+            for (DateTime day = firstDayNextMonth.Date; day.Date <= lastDayNextMonth.Date; day = day.AddDays(1))
+            {
+                datesList.Add(day);
+            }
+
+            var firstThursdays = datesList.Where(d => d.DayOfWeek == DayOfWeek.Thursday)
+                                           .GroupBy(d => d.Month)
+                                           .Select(e => e.First());
+
+            DateTime? lastThursday = firstThursdays.FirstOrDefault();
+            if (lastThursday != null)
+            {
+                LastWipe = lastThursday.Value.ToString(DateFormat);
+            }
+            else
+            {
+                LastWipe = "NoDateFound";
+            }
+
+            DateTime? nextThursday = firstThursdays.Skip(1).FirstOrDefault();
+            if (nextThursday != null)
+            {
+                NextWipe = nextThursday.Value.ToString(DateFormat);
+            }
+            else
+            {
+                NextWipe = "NoDateFound";
+            }
         }
+
         private void BroadcastWipe()
         {
             foreach (var p in BasePlayer.activePlayerList)
             {
-                SendReply(p, string.Format(MSG("MapWipe", p.UserIDString), LastWipe, NextWipeDays(NextWipeDate)));
-			}				
+                if (NextWipeDays(NextWipe) == "Today")
+                {
+                    SendReply(p, string.Format(msg("MapWipeToday", p.UserIDString), LastWipe, NextWipeDays(NextWipe)));
+                }
+                else
+                {
+                    SendReply(p, string.Format(msg("MapWipe", p.UserIDString), LastWipe, NextWipeDays(NextWipe)));
+                }
+            }                
         }
-        #endregion
 
-        #region ChatCommands
+        private string msg(string key, string id = null, params object[] args)
+        {
+            string message = id == null ? RemoveFormatting(lang.GetMessage(key, this, id)) : lang.GetMessage(key, this, id);
+
+            return args.Length > 0 ? string.Format(message, args) : message;
+        }
+
+        private string RemoveFormatting(string source)
+        {
+            return source.Contains(">") ? Regex.Replace(source, "<.*?>", string.Empty) : source;
+        }
+
+        ////////////////////////////////////////////////////////////
+        // Commands
+        ////////////////////////////////////////////////////////////
+
         [ChatCommand("wipe")]
         private void cmdNextWipe(BasePlayer player, string command, string[] args)
         {
-            SendReply(player, string.Format(MSG("MapWipe", player.UserIDString), LastWipe, NextWipeDays(NextWipeDate)));            
+            if (NextWipeDays(NextWipe) == "Today")
+            {
+                SendReply(player, string.Format(msg("MapWipeToday", player.UserIDString), LastWipe, NextWipeDays(NextWipe)));
+            }
+            else
+            {
+                SendReply(player, string.Format(msg("MapWipe", player.UserIDString), LastWipe, NextWipeDays(NextWipe)));
+            }                
         }
 
         [ConsoleCommand("wipe")]
         private void cmdGetWipe(ConsoleSystem.Arg arg)
         {
-            SendReply(arg, string.Format(MSG("MapWipe"), LastWipe, NextWipeDays(NextWipeDate)));
-        }
-
-        #endregion
-
-        #region Config        
-        private Configuration config;
-        public class Configuration
-        {
-            [JsonProperty(PropertyName = "Date format")]
-            public string DateFormat { get; set; } = "MM/dd/yyyy";
-            [JsonProperty(PropertyName = "Announce on join")]
-            public bool AnnounceOnJoin { get; set; } = false;
-            [JsonProperty(PropertyName = "Announce on timer")]
-            public bool AnnounceOnTimer { get; set; } = false;
-            [JsonProperty(PropertyName = "Announce timer")]
-            public int AnnounceTimer { get; set; } = 3;
-        }
-        private void LoadVariables()
-        {
-            if (string.IsNullOrEmpty(LastWipe)) {
-                DateTime dateTime = DateTime.Now;
-                DateTime firstDayMonth = new DateTime(dateTime.Year, dateTime.Month, 1);
-                DateTime lastDayMonth = firstDayMonth.AddMonths(1).AddDays(-1);
-
-                List<DateTime> dates = new List<DateTime>();
-                for (DateTime day =firstDayMonth.Date; day.Date <= lastDayMonth.Date; day = day.AddDays(1))
-                {
-                    dates.Add(day);
-                }
-                var query = dates.Where(d => d.DayOfWeek == DayOfWeek.Thursday).GroupBy(d => d.Month).Select(e => e.Take(3));
-                foreach (var item in query)
-                {
-                    foreach (var date in item)
-                    {
-                        LastWipe = date.ToString(config.DateFormat);
-                        break;
-                    }
-                }
-            }           
-            if (string.IsNullOrEmpty(NextWipe)) {
-                DateTime dateTime = DateTime.Now;
-                DateTime firstDayMonth = new DateTime(dateTime.Year, dateTime.Month, 1);
-                DateTime firstDayNextMonth = firstDayMonth.AddMonths(1);
-                DateTime lastDayNextMonth = firstDayMonth.AddMonths(2).AddDays(-1);
-
-                List<DateTime> dates = new List<DateTime>();
-                for (DateTime day =firstDayNextMonth.Date; day.Date <= lastDayNextMonth.Date; day = day.AddDays(1))
-                {
-                    dates.Add(day);
-                }
-                var query = dates.Where(d => d.DayOfWeek == DayOfWeek.Thursday).GroupBy(d => d.Month).Select(e => e.Take(3));
-                foreach (var item in query)
-                {
-                    foreach (var date in item)
-                    {
-                        NextWipe = date.ToString(config.DateFormat);
-                        break;
-                    }
-                }
-            }                   
-        }
-        protected override void LoadConfig()
-        {
-            base.LoadConfig();
-            try
+            if (NextWipeDays(NextWipe) == "Today")
             {
-                config = Config.ReadObject<Configuration>();
-                if (config == null)
-                {
-                    LoadDefaultConfig();
-                }
+                SendReply(arg, string.Format(msg("MapWipeToday"), LastWipe, NextWipeDays(NextWipe)));
             }
-            catch
+            else
             {
-                LoadDefaultConfig();
+                SendReply(arg, string.Format(msg("MapWipe"), LastWipe, NextWipeDays(NextWipe)));
             }
-            SaveConfig();
         }
-        protected override void LoadDefaultConfig()
-        {
-            config = new Configuration();
-        }
-        protected override void SaveConfig() => Config.WriteObject(config);
-        #endregion
 
-        #region Messaging
-        private string MSG(string key, string playerid = null) => lang.GetMessage(key, this, playerid);
+        ////////////////////////////////////////////////////////////
+        // Configs
+        ////////////////////////////////////////////////////////////
+
+        private bool ConfigChanged;
+        private string DateFormat;
+        private bool AnnounceOnJoin;
+        private bool AnnounceOnTimer;
+        private int AnnounceTimer;
+
+        protected override void LoadDefaultConfig() => PrintWarning("Generating default configuration file...");
+
+        private void InitConfig()
+        {
+            DateFormat = GetConfig("MM/dd/yyyy", "Date format");
+            AnnounceOnJoin = GetConfig(false, "Announce on join");
+            AnnounceOnTimer = GetConfig(false, "Announce on timer");
+            AnnounceTimer = GetConfig(3, "Announce timer");
+
+            if (ConfigChanged)
+            {
+                PrintWarning("Updated configuration file with new/changed values.");
+                SaveConfig();
+            }
+        }
+
+        private T GetConfig<T>(T defaultVal, params string[] path)
+        {
+            var data = Config.Get(path);
+            if (data != null)
+            {
+                return Config.ConvertValue<T>(data);
+            }
+
+            Config.Set(path.Concat(new object[] { defaultVal }).ToArray());
+            ConfigChanged = true;
+            return defaultVal;
+        }
 
         protected override void LoadDefaultMessages()
         {
             lang.RegisterMessages(new Dictionary<string, string>
             {            
-                {"MapWipe", "Last Map Wipe: <color=#ffae1a>{0}</color>\nTime Until Next Map Wipe: <color=#ffae1a>{1}</color>" }
+                {"MapWipe", "Last Map Wipe: <color=#ffae1a>{0}</color>\nTime Until Next Map Wipe: <color=#ffae1a>{1}</color>" },
+                {"MapWipeToday", "Last Map Wipe: <color=#ffae1a>{0}</color>\nTime Until Next Map Wipe: <color=#ffae1a>today (19:00 UTC)</color>" }
             }, this);
             lang.RegisterMessages(new Dictionary<string, string>
             {            
-                {"MapWipe", "Ultimul Wipe de Mapă: <color=#ffae1a>{0}</color>\nTimpul până la urmâtorul Wipe de Mapă: <color=#ffae1a>{1}</color>" }
+                {"MapWipe", "Ultimul Wipe de Mapă: <color=#ffae1a>{0}</color>\nTimpul până la urmâtorul Wipe de Mapă: <color=#ffae1a>{1}</color>" },
+                {"TMapWipeoday", "Ultimul Wipe de Mapă: <color=#ffae1a>{0}</color>\nTimpul până la urmâtorul Wipe de Mapă: <color=#ffae1a>astăzi (19:00 UTC)</color>" }
             }, this, "ro");
         }
-        #endregion
     }
 }
